4a952c3e191eac1ca8cf0fede94ca3e5f69a253d
Keith Daulton <keith.daulton@gitkraken.com>
2023-05-30 10:32:15 -0400

    Removes extra closing brace

--- src/webviews/apps/plus/shared/components/vscode.css.ts
+++ src/webviews/apps/plus/shared/components/vscode.css.ts
@@ -1,16 +1,15 @@
 import { css } from 'lit';
 
 export const linkStyles = css`
-		a {
-			color: var(--link-foreground);
-			text-decoration: none;
-		}
-		a:focus {
-			outline-color: var(--focus-border);
-		}
-		a:hover {
-			color: var(--link-foreground-active);
-			text-decoration: underline;
-		}
+	a {
+		color: var(--link-foreground);
+		text-decoration: none;
+	}
+	a:focus {
+		outline-color: var(--focus-border);
+	}
+	a:hover {
+		color: var(--link-foreground-active);
+		text-decoration: underline;
 	}
 `;

28ff95ee1c97f4eef073cf4935c49099ad75b322
Eric Amodio <eamodio@gmail.com>
2023-08-15 04:03:48 -0400

    Adds getDiffFiles to get set of files in the patch Adds open patch command

--- src/constants.ts
+++ src/constants.ts
@@ -185,6 +185,7 @@ export const enum Commands {
 	OpenFolderHistory = 'gitlens.openFolderHistory',
 	OpenOnRemote = 'gitlens.openOnRemote',
 	OpenIssueOnRemote = 'gitlens.openIssueOnRemote',
+	OpenPatch = 'gitlens.openPatch',
 	OpenPullRequestOnRemote = 'gitlens.openPullRequestOnRemote',
 	OpenAssociatedPullRequestOnRemote = 'gitlens.openAssociatedPullRequestOnRemote',
 	OpenRepoOnRemote = 'gitlens.openRepoOnRemote',
